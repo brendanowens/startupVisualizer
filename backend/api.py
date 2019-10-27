b0e46534bb1a7e1dd4e4de9d709b7a86e4f4f00b
Brendan Owens <brendan@huskers.unl.edu>
2019-10-26 22:12:10 -0500

    filter fields all

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,6 @@ from backend.serializers import CompanySerializer
 
 class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
-    filter_fields = ('us_state',)
 
     def get_queryset(self):
         return Company.objects.all()
