6735d074241417a6987974168c2492a410fcbc9a
Brendan Owens <brendan@huskers.unl.edu>
2019-12-10 14:24:57 -0600

    add geos paths

--- startupData/settings.py
+++ startupData/settings.py
@@ -160,6 +160,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if on_heroku:
+    GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
+    GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
     ALLOWED_HOSTS = ['*']
     config = locals()
     django_heroku.settings(config, databases=False)
